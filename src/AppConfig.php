8ca2559877486c600bfa355f976b84b31258c314
e <andy@feys.be>
2026-06-30 10:07:53 +0200

    moved the AppConfig::loadConfig() call to AppRunner::init() + added loads of debug messages.

--- src/AppConfig.php
+++ src/AppConfig.php
@@ -19,21 +19,22 @@ class AppConfig
     // On the server the file config.php has to be read only to avoid being overwritten when all files are ftp'd to the live server
     public static function loadConfig()
     {
+        echo "AppConfig::loadConfig called<br>";
         $configPath = __DIR__ . '/config.php'; // Or config.json
         if (file_exists($configPath)) {
+            echo "config.php file exists<br>";
             $config = include $configPath; // Load PHP array config
 
             foreach ($config as $key => $value) {
                 if (property_exists(__CLASS__, $key)) {
                     self::$$key = $value; // Dynamically assign properties
+                    echo "$key = $value";
                 }
             }
 
-            // making sure that all relevant libraries are loaded
-            require_once(AppConfig::get("BaseDirectory") . '/vendor/autoload.php');
-
             // loading the database connections in the ConnectionManager
             if (Self::keyExists("DBConnections")) {
+echo "There are dbconnections defined:<br>";
                 // creating the connection manager
                 $connmgr = \FL\ConnectionManager::getInstance();
                 $connections = AppConfig::get("DBConnections");
@@ -57,13 +58,17 @@ class AppConfig
                     $connmgr->addConnection(
                             \FL\Connection::getInstance($name, $host, $user, $pwd, $dbname)
                         );
+                    echo "$name/$host/$user/$pwd/$dbname<br>";
                 }
             }
 
             // loading the model directories
             if (Self::keyExists("DBModelDirs")) {
-                foreach( AppConfig::get("DBModelDirs") as $dir)
-                \FL\Model::initialize(array(AppConfig::$BaseDirectory .$dir));
+                echo "There are modeldirs defined<br>";
+                foreach( AppConfig::get("DBModelDirs") as $dir) {
+                    \FL\Model::initialize(array(AppConfig::$BaseDirectory . $dir));
+                    echo AppConfig::$BaseDirectory . $dir . "<br>";
+                }
             }
         }
     }
@@ -90,4 +95,4 @@ class AppConfig
         return property_exists(__CLASS__, $key);
     }
 }
-AppConfig::loadConfig();
+
--- src/AppRunner.php
+++ src/AppRunner.php
@@ -2,6 +2,9 @@
 namespace FL;
 class AppRunner
 {
+    public static function init() {
+        AppConfig::loadConfig();
+    }
     public static function run() {
         echo "<pre>";
         print_r(AppConfig::getConfig());
