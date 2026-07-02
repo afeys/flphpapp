782191868d73ba372a0cc5e458ab9687a1d1cd82
e <andy@feys.be>
2026-07-02 12:12:59 +0200

    added a way to load views.

--- src/AppRunner.php
+++ src/AppRunner.php
@@ -10,9 +10,16 @@ class AppRunner
     public static function init($configPath) {
         AppConfig::loadConfig($configPath);
     }
-    public static function run() {
+    public static function run($view = "") {
         echo "<pre>";
         print_r(AppConfig::getConfig());
         echo "</pre>";
+        $viewDir = AppConfig::get("ViewDir");
+        if ($view !== "") {
+            $viewFile = AppConfig::get("BaseDirectory") .  $viewDir . "/" . str_replace('.', '/', $view) . ".php";
+            if (file_exists($viewFile)) {
+                require_once $viewFile;
+            }
+        }
     }
 }
